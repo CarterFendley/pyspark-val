30d653c580644b822e5ee15058925311c0823a17
carter.fendley <carter.fendley@gmail.com>
2024-01-18 22:11:50 -0500

    Add docstrings & extra validation

--- pyspark_val/mock.py
+++ pyspark_val/mock.py
@@ -4,9 +4,57 @@ from unittest.mock import Mock
 from pyspark.sql.readwriter import PathOrPaths
 
 def mock_read_parquet(paths: Dict[str, Any], mock: Mock = None) -> Mock:
+    """
+    This function provides an facility for mocking `spark.read.parquet` calls. See the example below.
+
+    ```python
+    paths = {
+        'file/one.parquet': '_value_one_',
+        'file/two.parquet': '_value_two_'
+    }
+    spark = mock_read_parquet(paths)
+
+    spark.read.parquet('file/one.parquet')
+    >>> '_value_one_'
+    spark.read.parquet('file/two.parquet')
+    >>> '_value_two_'
+    ```
+
+    Args:
+        type (str): The type of read to mock.
+        paths (Dict[str, Any]): A dictionary containing a mapping from input paths to objects returned.
+        mock (Mock, optional): Existing mock object. Defaults to None.
+
+    Returns:
+        Mock: The mock with functionality displayed above
+    """
     return mock_read('parquet', paths, mock)
 
 def mock_read(type: str, paths: Dict[str, Any], mock: Mock = None) -> Mock:
+    """
+    This function provides a facility for mocking `spark.read` calls. See the example below.
+
+    ```python
+    paths = {
+        'file/one.parquet': '_value_one_',
+        'file/two.parquet': '_value_two_'
+    }
+    spark = mock_read('parquet', paths)
+
+    spark.read.parquet('file/one.parquet')
+    >>> '_value_one_'
+    spark.read.parquet('file/two.parquet')
+    >>> '_value_two_'
+    ```
+
+    Args:
+        type (str): The type of read to mock.
+        paths (Dict[str, Any]): A dictionary containing a mapping from input paths to objects returned.
+        mock (Mock, optional): Existing mock object. Defaults to None.
+
+    Returns:
+        Mock: The mock with functionality displayed above
+    """
     if mock is None:
         mock = Mock()
 
